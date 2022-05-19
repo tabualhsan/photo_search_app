87979ca095ce918e27d36adbb905032b1190ca58
tamara abualhsan <tabua@Tamaras-MacBook-Pro.local>
2022-05-19 15:02:48 -0700

    added api, css and styling

--- app.js
+++ app.js
@@ -6,14 +6,22 @@ const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
 const form = document.querySelector('.search-form');
 let searchValue;
+const more = document.querySelector(".more");
+let page = 1;
+let fetchLink;
+let currentSearch;
 
 // event listenrs 
 
 searchInput.addEventListener('input', updateInput);
 form.addEventListener('submit', (e) => {
     e.preventDefault(); 
+    currentSearch= searchValue;
     searchPhotos(searchValue)
-})
+});
+
+more.addEventListener('click', loadMore);
+
 function updateInput(e){
     searchValue = e.target.value;
 
@@ -36,15 +44,21 @@ function generatePictures(data){
     data.photos.forEach(photo => {
         const galleryImg = document.createElement('div');
         galleryImg.classList.add('gallery-img');
-        galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-        <p>${photo.photographer}</p>`;
-        gallery.appendChild(galleryImg);
+        galleryImg.innerHTML = `
+        <div class="gallery-info">
+        <p>${photo.photographer}</p>
+        <a href=${photo.src.original}>Download</a>
+        </div>
+        <img src=${photo.src.large}></img>
+        `;
+        gallery.appendChild(galleryImg) ;
 
     });  
 }
 
 async function curatedPhotos(){
-   const data = await fetchApi("https://api.pexels.com/v1/curated");
+    fetchLink = "https://api.pexels.com/v1/curated"
+   const data = await fetchApi(fetchLink);
    generatePictures(data);
 
 
@@ -53,16 +67,29 @@ async function curatedPhotos(){
 
 async function searchPhotos(query){
     clear();
-    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}`);
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}`
+    const data = await fetchApi(fetchLink);
     generatePictures(data);
 }
 
-// this filters it to the search 
+
+
+// this filters it to the search and clears the search area after you search
 function clear(){
     gallery.innerHTML = "";
-    searchInput.value = '';
+    searchInput.value = "";
 }
 
+async function loadMore(){
+    page++;
+    if(currentSearch){
+        fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page15&page=${page}`    
+    }else{
+        fetchLink=`https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+    }
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
+}
 curatedPhotos();
 
 
